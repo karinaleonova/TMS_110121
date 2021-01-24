a8921ad631593f4cdb97a58beccc93bbef78819f
mirana <mirana@miranas-MacBook-Pro.local>
2021-01-24 23:37:41 +0100

    task8

---
+++ Task8.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+//Имеется целове число (задать с помощью Random rand = new Random(); int x =
+//rand.nextInt() ). Это число – количесво денег в рублях. Вывести это число, добавив к
+//нему слово «рублей» в правильном падеже.
+ 
+
+
+public class Task8 {
+    public static void main(String[] args){
+
+        Random rand = new Random();
+        int x = rand.nextInt(); 
+    
+        
+         if ( x == 1 ){
+            System.out.println(x + " рубль");
+        }
+         else if ( x >= 2 && x <= 4 ){
+            System.out.println(x + " рубля");
+        }
+         else if ( x > 0 ) {
+            System.out.println(x + " рублей");
+        }
+    }
+}
+
+    
+
+
