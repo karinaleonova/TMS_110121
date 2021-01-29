96d4dc84dd3ca065c3c39659814f1d6730c1e3fe
mirana <mirana@miranas-MacBook-Pro.local>
2021-01-29 22:25:08 +0100

    fixTask6

--- Task6.java
+++ Task6.java
@@ -4,28 +4,23 @@
 
 public class Task6 {
     public static void main(String[] args){
-        int i = 137; 
+        int i = 1142; 
 
         if ( i >=100 && i <= 999 ){
             System.out.println("This is a three-digit number ");
         }
+        else System.out.println("Isn't a three-digit number ");
+
         if ( i % 2 == 0){
             System.out.println("This is an even number ");
         }
-        int l = i / 10;
-        l = i - l * 10;
-        if( l == 7){
-            System.out.println("The last number is 7");
-        }
+        else System.out.println("Isn't an even number ");
+
         if( i % 10 == 7){
             System.out.println("The last number is 7");
         }
+        else System.out.println("The last number is not 7");
         
     }
     
 }
-
-/*  int i = 132;
-    int n1 = i / 10;
-    n1 = i - n1 * 10;
-    return n1;*/
