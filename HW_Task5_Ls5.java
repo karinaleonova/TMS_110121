bcb361a39f9daac0a7885d5c33e92ae991720842
mirana <mirana@miranas-MacBook-Pro.local>
2021-02-09 22:07:41 +0100

    fix

--- HW_Task5_Ls5.java
+++ HW_Task5_Ls5.java
@@ -5,13 +5,12 @@ public class HW_Task5_Ls5 {
 
     public static void main(String[] args) {
 
-        double[] num = new double[20];
-        for (double i = 0; i <= num.length; i++) {
-            num[(int) i] = i + 1;
+        double count = 1;
+        for (double i = 1; i <= 20; i++) {
 
-            num[(int) i] *= 2.54;
+            count = i * 2.54;
 
-            System.out.println(i + 1 + " inch = " + num[(int) i] + " centimetr ");
+            System.out.println(i + " inch = " + count + " centimetr ");
         }
 
     }
--- HW_Task7_Ls5.java
+++ HW_Task7_Ls5.java
@@ -3,13 +3,12 @@
 public class HW_Task7_Ls5 {
     public static void main(String[] args) {
 
-        int[] num = new int[99];
+        
         int count = 0;
-        for (int i = 0; i < num.length; i++) {
-            num[i] = i + 1;
-            if (num[i] % 2 != 0) {
+        for (int i = 0; i < 9; i++) {
+            if (i % 2 != 0) {
 
-                count += num[i];
+                count += i;
 
             }
 
