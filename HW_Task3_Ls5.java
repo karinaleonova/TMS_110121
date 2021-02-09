334fbbd3000af82921179d4693c8fb923b92787d
mirana <mirana@miranas-MacBook-Pro.local>
2021-02-09 22:07:41 +0100

    fix

--- HW_Task3_Ls5.java
+++ HW_Task3_Ls5.java
@@ -2,17 +2,12 @@
 
 public class HW_Task3_Ls5 {
     public static void main(String[] args) {
-        int[] num = new int[256];
+
         int count = 0;
-        for (int i = 0; i < num.length; i++) {
-            num[i] = i + 1;
-
-            if (num[i] % 2 == 0) {
-
-                count += num[i];
-
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                count += i;
             }
-
         }
         System.out.println(count + 1);
     }
--- HW_Task6_Ls5.java
+++ HW_Task6_Ls5.java
@@ -4,9 +4,7 @@
 public class HW_Task6_Ls5 {
     public static void main(String[] args) {
         
-
-        int [] num = new int[100];
-        for (int i = 1 ; i <= num.length; i++){
+        for (int i = 2 ; i <= 100; i++){
             if(i % 2 == 0 ){
                 System.out.println(i);
             }
