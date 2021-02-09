9949f8bf333e021eaf221c2f5b618d3475dcaa2c
mirana <mirana@miranas-MacBook-Pro.local>
2021-02-09 20:34:00 +0100

    fix

--- Game.java
+++ Game.java
@@ -1,15 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) throws Exception {
 
+        Random random = new Random();
         Scanner s = new Scanner(System.in);
         String[][] gridIndex = { { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" }, { "5", "5" }, { "6", "6" },
                 { "7", "7" }, { "8", "8" }, { "9", "9" } };
 
         boolean stopTheGame = false;
 
+
+        
         while (!stopTheGame) {
+           
 
             for (int i = 0; i < gridIndex.length; i++) {
                 System.out.print(gridIndex[i][1] + " ");
@@ -19,6 +24,7 @@ public class Game {
                 if (i == 0 || i == 1 || i == 3 || i == 4 || i == 6 || i == 7)
                     System.out.print("| ");
             }
+
 
             System.out.println("\n\nWhere do you want to put 'X' ?");
             System.out.println("Only numbers from 1-9 :");
@@ -36,65 +42,67 @@ public class Game {
                 gridIndex[Integer.parseInt(userInput) - 1][1] = "X";
                 System.out.println("\n");
 
-                int count = 0;
-                for (int i = 0; i < 8; i++) {
+            int count = 0;
+            for (int i = 0; i < 8; i++) {
 
-                    if ((gridIndex[0][1].equals("O") && gridIndex[1][1].equals("O") && gridIndex[2][1].equals("O"))
-                            || (gridIndex[3][1].equals("O") && gridIndex[4][1].equals("O")
-                                    && gridIndex[5][1].equals("O"))
-                            || (gridIndex[6][1].equals("O") && gridIndex[7][1].equals("O")
-                                    && gridIndex[8][1].equals("O"))
-                            || (gridIndex[0][1].equals("O") && gridIndex[3][1].equals("O")
-                                    && gridIndex[6][1].equals("O"))
-                            || (gridIndex[1][1].equals("O") && gridIndex[4][1].equals("O")
-                                    && gridIndex[7][1].equals("O"))
-                            || (gridIndex[2][1].equals("O") && gridIndex[5][1].equals("O")
-                                    && gridIndex[8][1].equals("O"))
-                            || (gridIndex[0][1].equals("O") && gridIndex[4][1].equals("O")
-                                    && gridIndex[8][1].equals("O"))
-                            || (gridIndex[2][1].equals("O") && gridIndex[4][1].equals("O")
-                                    && gridIndex[6][1].equals("O"))) {
-                        System.out.println("\n0 PLAYER WON!!!\n");
-                        stopTheGame = true;
-                        break;
 
-                    }
-                    if ((gridIndex[0][1].equals("X") && gridIndex[1][1].equals("X") && gridIndex[2][1].equals("X"))
-                            || (gridIndex[3][1].equals("X") && gridIndex[4][1].equals("X")
-                                    && gridIndex[5][1].equals("X"))
-                            || (gridIndex[6][1].equals("X") && gridIndex[7][1].equals("X")
-                                    && gridIndex[8][1].equals("X"))
-                            || (gridIndex[0][1].equals("X") && gridIndex[3][1].equals("X")
-                                    && gridIndex[6][1].equals("X"))
-                            || (gridIndex[1][1].equals("X") && gridIndex[4][1].equals("X")
-                                    && gridIndex[7][1].equals("X"))
-                            || (gridIndex[2][1].equals("X") && gridIndex[5][1].equals("X")
-                                    && gridIndex[8][1].equals("X"))
-                            || (gridIndex[0][1].equals("X") && gridIndex[4][1].equals("X")
-                                    && gridIndex[8][1].equals("X"))
-                            || (gridIndex[2][1].equals("X") && gridIndex[4][1].equals("X")
-                                    && gridIndex[6][1].equals("X"))) {
-                        System.out.println("\nX PLAYER WON!!!\n");
+                int rand = random.nextInt(8);
+                if (!gridIndex[rand][1].equals("X") && !gridIndex[rand][1].equals("O") && !gridIndex[rand][1].equals(userInput) ) {
+                    gridIndex[rand][1] = "O";
+                    break;
+                }
+                if ((gridIndex[0][1].equals("O") && gridIndex[1][1].equals("O") && gridIndex[2][1].equals("O"))
+                        || (gridIndex[3][1].equals("O") && gridIndex[4][1].equals("O")
+                                && gridIndex[5][1].equals("O"))
+                        || (gridIndex[6][1].equals("O") && gridIndex[7][1].equals("O")
+                                && gridIndex[8][1].equals("O"))
+                        || (gridIndex[0][1].equals("O") && gridIndex[3][1].equals("O")
+                                && gridIndex[6][1].equals("O"))
+                        || (gridIndex[1][1].equals("O") && gridIndex[4][1].equals("O")
+                                && gridIndex[7][1].equals("O"))
+                        || (gridIndex[2][1].equals("O") && gridIndex[5][1].equals("O")
+                                && gridIndex[8][1].equals("O"))
+                        || (gridIndex[0][1].equals("O") && gridIndex[4][1].equals("O")
+                                && gridIndex[8][1].equals("O"))
+                        || (gridIndex[2][1].equals("O") && gridIndex[4][1].equals("O")
+                                && gridIndex[6][1].equals("O"))) {
+                    System.out.println("\nCOMPUTER WINS!!!\n");
+                    stopTheGame = true;
+                    break;
 
-                        stopTheGame = true;
-                        break;
-                    }
+                }
+                if ((gridIndex[0][1].equals("X") && gridIndex[1][1].equals("X") && gridIndex[2][1].equals("X"))
+                        || (gridIndex[3][1].equals("X") && gridIndex[4][1].equals("X")
+                                && gridIndex[5][1].equals("X"))
+                        || (gridIndex[6][1].equals("X") && gridIndex[7][1].equals("X")
+                                && gridIndex[8][1].equals("X"))
+                        || (gridIndex[0][1].equals("X") && gridIndex[3][1].equals("X")
+                                && gridIndex[6][1].equals("X"))
+                        || (gridIndex[1][1].equals("X") && gridIndex[4][1].equals("X")
+                                && gridIndex[7][1].equals("X"))
+                        || (gridIndex[2][1].equals("X") && gridIndex[5][1].equals("X")
+                                && gridIndex[8][1].equals("X"))
+                        || (gridIndex[0][1].equals("X") && gridIndex[4][1].equals("X")
+                                && gridIndex[8][1].equals("X"))
+                        || (gridIndex[2][1].equals("X") && gridIndex[4][1].equals("X")
+                                && gridIndex[6][1].equals("X"))) {
+                    System.out.println("\nPLAYER WINS!!!\n");
+                    stopTheGame = true;
+                    break;
+                }
+   
 
-                    if (!gridIndex[i][1].equals("X") && !gridIndex[i][1].equals("O")) {
-                        gridIndex[i][1] = "O";
+                count++;
+            }
 
-                        break;
 
-                    }
 
-                    count++;
 
-                    if (count == 8) {
-                        System.out.println("\nNO WINNER!!!\n");
-                        stopTheGame = true;
-                        break;
-                    }
 
+                if (count == 9) {
+                    System.out.println("\nNO WINNER!!!\n");
+                    stopTheGame = true;
+                    break;
                 }
 
             }
@@ -102,4 +110,3 @@ public class Game {
 
     }
 }
-
